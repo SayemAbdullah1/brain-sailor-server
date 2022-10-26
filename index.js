227d7f2b477602eeafcc9bcc23989db94b558925
SayemAbdullah1 <dxsayem0@gmail.com>
2022-10-26 18:54:38 +0600

    server site running on port 5000

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('News API running')
+});
+
+app.listen(port, () => {
+    console.log('Dragon News server running on port', port)
+})
